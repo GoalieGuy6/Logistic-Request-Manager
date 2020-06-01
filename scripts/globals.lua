7764bbcf68f5ac7c0d1428b69eb36cecf69e8231
gorch <zentgrafcn@gmail.com>
2020-06-02 01:01:19 +0200

    Update globals.lua
    added screen location to global table

--- scripts/globals.lua
+++ scripts/globals.lua
@@ -1,18 +1,20 @@
 if not globals then globals = {} end
 
 function globals.init()
-	global = global or {}
-	global["preset-data"] = global["preset-data"] or {}
-	global["preset-names"] = global["preset-names"] or {}
-	global["presets-selected"] = global["presets-selected"] or {}
-	global["inventories-open"] = global["inventories-open"] or {}
+	global 						= global or {}
+	global["preset-data"]		= global["preset-data"] or {}
+	global["preset-names"]		= global["preset-names"] or {}
+	global["presets-selected"]	= global["presets-selected"] or {}
+	global["inventories-open"]	= global["inventories-open"] or {}
+	global["screen_location"]	= global["screen_location"] or {}
 	global.on_tick = global.on_tick or false
 end
 
 function globals.init_player(player)
 	local index = player.index
 	
-	global["preset-data"][index] = global["preset-data"][index] or {}
-	global["preset-names"][index] = global["preset-names"][index] or {}
-	global["presets-selected"][index] = global["presets-selected"][index] or 0
+	global["preset-data"][index] 		= global["preset-data"][index] or {}
+	global["preset-names"][index] 		= global["preset-names"][index] or {}
+	global["presets-selected"][index] 	= global["presets-selected"][index] or 0
+	global["screen_location"][index] 	= global["screen_location"][index] or {}
 end
