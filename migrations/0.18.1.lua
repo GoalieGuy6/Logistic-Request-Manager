268813b2a799cb94c54d5d9da3fb94a71bbb904d
gorch <zentgrafcn@gmail.com>
2020-06-06 17:51:01 +0200

    fixed migrations
    - removed potential nil references
    - included gui.destroy_old() completely

--- migrations/0.18.1.lua
+++ migrations/0.18.1.lua
@@ -1,18 +1,26 @@
 local mod_gui = require 'mod-gui'
 
 for _, player in pairs(game.players) do
-    -- remove old static gui from gui.top
-    local old_frame=mod_gui.get_frame_flow(player)
-                        ["logistic-request-manager-gui-frame"]
-    if old_frame then 
-        old_frame.destroy() 
+    -- remove interim-version of floating screen - should never have been created for anybody but during testing (just playing it save as the old version was on github...)
+    if player.gui.screen["logistic-request-manager-gui-frame"] then
+        player.gui.screen["logistic-request-manager-gui-frame"].destroy()
     end
 
+    -- remove old static gui from gui.left
+	local frame_flow = mod_gui.get_frame_flow(player)
+    if frame_flow["logistic-request-manager-gui-frame"] then
+        frame_flow["logistic-request-manager-gui-frame"].destroy()
+    end
+
+    -- remove old unused frame from gui.left
+    if frame_flow["logistic-request-manager-gui"] then
+        frame_flow["logistic-request-manager-gui"].destroy()
+    end
+    
     -- remove old unused button
-    local old_button_flow=mod_gui.get_button_flow(player)
-                            ["logistic-request-manager-button"]
-    if old_button_flow then
-        old_button_flow.destroy()
+    local button_flow = mod_gui.get_button_flow(player)
+    if button_flow["logistic-request-manager-button"] then
+        button_flow["logistic-request-manager-button"].destroy()
     end
 
     player.print("[LRM] Migration to 0.18.1 complete.")
