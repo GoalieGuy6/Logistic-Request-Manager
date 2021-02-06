06847adb753143b8bc4677363df91878ef705e81
Daeruun <zentgrafcn@gmail.com>
2021-02-06 11:06:50 +0100

    added new custom input
    backport from 1.1.5

--- data.lua
+++ data.lua
@@ -1,3 +1,11 @@
 require 'defines'
 
 require 'prototypes/style'
+
+local button={
+    type = "custom-input",
+    name = "LRM-input-toggle-gui",
+    key_sequence = "SHIFT + L",
+    consuming = "none"
+}
+data:extend{button}
