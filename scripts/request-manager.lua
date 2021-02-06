c32eaca9275dcba7bfdc90378834ce95c63fc1c3
Daeruun <zentgrafcn@gmail.com>
2021-02-06 11:01:16 +0100

    fixed chests to be valid targets
    backport from 1.1.5

--- scripts/request-manager.lua
+++ scripts/request-manager.lua
@@ -1,7 +1,14 @@
 if not request_manager then request_manager = {} end
 
-function request_manager.request_blueprint(player, entity)
-	entity = entity or player.character
+function request_manager.request_blueprint(player)
+	-- if not (player.is_cursor_blueprint()) then 
+	-- 	return nil 
+	-- end
+
+	local entity = get_inventory_entity(player, {"messages.target-entity"}, {"messages.append"}, {"messages.blueprint"})
+	if not (entity and entity.valid) then
+		return nil
+	end
 	local blueprint = player.cursor_stack
 	
 	if not (blueprint and blueprint.valid and blueprint.valid_for_read) then
@@ -66,6 +73,37 @@ function request_manager.request_blueprint(player, entity)
 end
 
 function request_manager.apply_preset(preset_data, entity)
+	local logistic_point = entity.get_logistic_point(defines.logistic_member_index.character_requester)
+	if (	not (logistic_point.mode == defines.logistic_mode.requester ) 			-- no requester
+		and not (logistic_point.mode == defines.logistic_mode.buffer ) 	) then		-- no buffer
+		return nil
+	end
+
+	logistic_point = entity.get_logistic_point(defines.logistic_member_index.character_provider)
+	if not (logistic_point) then 						-- no auto-trash
+		set_slot = entity.set_request_slot
+	else
+		if entity.type == "character" then		-- easy & quite certain
+			set_slot = entity.set_personal_logistic_slot 
+		else											-- spidertron OK & quite sure that this will work for modded vehicles as well...
+			set_slot = entity.set_vehicle_logistic_slot
+		end
+	end
+
+	if not set_slot then 
+		return nil 
+	end
+
+	-- clear current logistic slots
+	local slots = entity.request_slot_count
+
+	for i = 1, slots do
+		entity.clear_request_slot(i)
+	end
+
+	-- get required number of personal logistic slots
+	slots = table_size(preset_data)
+	
 	-- as only players personal logistic slots support min & max requests, we need to destinguish between player-character and entities like requester-box or similar
 	if entity.type == "character" then
 		-- clear current personal logistic slots
@@ -102,6 +140,11 @@ function request_manager.apply_preset(preset_data, entity)
 end
 
 function request_manager.save_preset(player, preset_number, preset_name)
+	local entity = get_inventory_entity(player, {"messages.source-entity"}, {"messages.save"}, {"messages.preset"})
+	if not (entity and entity.valid) then
+		return nil
+	end
+
 	local player_presets = global["preset-names"][player.index]
 	local total = 0
 	for number, name in pairs(player_presets) do
@@ -114,35 +157,57 @@ function request_manager.save_preset(player, preset_number, preset_name)
 	if preset_number == 0 then
 		preset_number = total + 1
 	end
+
+	local request_data = {}
+	local slots = entity.request_slot_count
+	if slots % 10 then
+		slots = slots + 10 - (slots % 10)
+	end
+	if slots < 40 then 
+		slots = 40
+	end
+	local get_slot = nil
+
+	local logistic_point = entity.get_logistic_point(defines.logistic_member_index.character_provider) 
+	if not (logistic_point) then 						-- no auto-trash
+		get_slot = entity.get_request_slot
+	else
+		if entity.type == "character" then				-- easy & quite certain
+			get_slot = entity.get_personal_logistic_slot 
+		end
+	end
+
+	if not get_slot then 
+		return nil 
+	end
 	
 	request_data = {}
-	local slots = player.character_logistic_slot_count
+	local slots = entity.request_slot_count
 	for i = 1, slots do
-		local request = player.get_personal_logistic_slot(i)
+		local request = get_slot(i) -- .get_personal_logistic_slot(i)
 		if request and request.name then
-			request_data[i] = { name = request.name, min = request.min, max = request.max }
+			request_data[i] = { name = request.name, min = request.min or request.count, max = request.max or 0xFFFFFFFF }
 		else
 			request_data[i] = { nil }
 		end
 	end
 	
 	global["preset-names"][player.index][preset_number] = preset_name
-	global["preset-data"][player.index][preset_number] = request_data
+	global["preset-data"][player.index][preset_number]  = request_data
 	
 	return preset_number
 end
 
 function request_manager.load_preset(player, preset_number)
 	local player_presets = global["preset-data"][player.index]
-	preset = player_presets[preset_number]
+	local preset = player_presets[preset_number]
 	if not preset then return end
 	
-	local chest_open = global["inventories-open"][player.index]
-	
-	if chest_open and chest_open.valid then
-		request_manager.apply_preset(preset, chest_open)
+	local entity = get_inventory_entity(player, {"messages.target-entity"}, {"messages.load"}, {"messages.preset"})
+	if entity and entity.valid then
+		request_manager.apply_preset(preset, entity)
 	else
-		request_manager.apply_preset(preset, player.character)
+		return nil
 	end
 end
 
