b00a24de9d50e02f9d3026c987317bbfde033a5c
Daeruun <zentgrafcn@gmail.com>
2021-12-15 21:45:54 +0100

    added a command to create a new temlate named 'keep_all'

--- scripts/commands.lua
+++ scripts/commands.lua
@@ -3,7 +3,7 @@ if not lrm.commands then
     lrm.commands = {}
 end
 
-lrm.commands.commands="force_gui, renew_empty, renew_autotrash"
+lrm.commands.commands="force_gui, renew_empty, renew_autotrash, renew_keep_all"
 lrm.commands.help={"", " /lrm help [", {"command.parameter"}, "]\n ", {"command.with_parameters"}, " ", lrm.commands.commands}
 lrm.commands.details={"", {"command.details"}, lrm.commands.help}
 lrm.commands.usage={"", {"command.usage"}, "  /lrm [", {"command.parameter"}, "]\n ", {"command.with_parameters"}, " help, ", lrm.commands.commands, "\n ", lrm.commands.details}
@@ -33,6 +33,9 @@ function lrm.commands.run(event)
         elseif parameters[1]=="renew_autotrash" then
             lrm.recreate_autotrash_preset(player)
 
+        elseif parameters[1]=="renew_keep_all" then
+            lrm.recreate_keep_all_preset(player)
+
         elseif parameters[1]=="force_gui" then
             lrm.gui.build_toggle_button(player)
         else
@@ -52,6 +55,8 @@ function lrm.commands.help(player, parameters)
         lrm.message(player, {"", {"details-renew_empty"} })
     elseif parameters[2]=="renew_autotrash" then
         lrm.message(player, {"", {"details-renew_autotrash"} })
+    elseif parameters[2]=="renew_keep_all" then
+        lrm.message(player, {"", {"details-renew_keep_all"} })
     elseif parameters[2]=="force_gui" then
         lrm.message(player, {"", {"details-force_gui"} })
     else
