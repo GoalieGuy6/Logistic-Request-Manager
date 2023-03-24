ee582a9abd6ed11184384c5c9e4cd82acc798c15
Daeruun <zentgrafcn@gmail.com>
2023-03-24 19:35:57 +0100

    added globals for autotrash handling

--- scripts/globals.lua
+++ scripts/globals.lua
@@ -9,6 +9,9 @@ function lrm.globals.init()
     global["screen_location"]  = global["screen_location"] or {}
     global["bring_to_front"]   = global["bring_to_front"] or {}
     global["data_to_view"]     = global["data_to_view"] or {}
+    global["flags"]            = global["flags"] or {}
+    global["trash"]            = global["trash"] or {}
+    
     
     global.on_tick = global.on_tick or false
     
@@ -22,4 +25,18 @@ function lrm.globals.init_player(player)
     global["preset-names"][index]     = global["preset-names"][index] or {}
     global["presets-selected"][index] = global["presets-selected"][index] or 0
     global["screen_location"][index]  = global["screen_location"][index] or nil
+    global["flags"][index]            = global["flags"][index] or {
+                                                                    autotrash = false, 
+                                                                    autotrash_automatic = true,
+                                                                    trash_selection_tools = false, 
+                                                                    trash_blueprints = false, 
+                                                                    trash_blueprint_books = false, 
+                                                                    trash_deconstruction_planers = false, 
+                                                                    trash_upgrade_planers = false, 
+                                                                    trash_copy_paste_tools = false, 
+                                                                    trash_hidden_items = false, 
+                                                                    trash_gridded_items = false, 
+                                                                    trash_remotes = false
+                                                                }
+    global["trash"][index]            = global["trash"][index] or {}
 end
